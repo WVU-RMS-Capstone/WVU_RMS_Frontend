fe57c69d5eef03d23b9bc057e7d5568c09d268a4
Michael Hogue <michael.hogue.19@gmail.com>
2024-02-17 12:23:02 -0500

    [WIP] Fix ios packaging issues

--- metro.config.js
+++ metro.config.js
@@ -1,16 +1,11 @@
 const {getDefaultConfig, mergeConfig} = require('@react-native/metro-config');
 
-module.exports = function (baseConfig) {
-  const defaultConfig = mergeConfig(baseConfig, getDefaultConfig(__dirname));
-  const {resolver: {assetExts, sourceExts}} = defaultConfig;
+/**
+ * Metro configuration
+ * https://facebook.github.io/metro/docs/configuration
+ *
+ * @type {import('metro-config').MetroConfig}
+ */
+const config = {};
 
-  return mergeConfig(
-    defaultConfig,
-    {
-      resolver: {
-        assetExts: assetExts.filter(ext => ext !== 'svg'),
-        sourceExts: [...sourceExts, 'svg'],
-      },
-    },
-  );
-};
+module.exports = mergeConfig(getDefaultConfig(__dirname), config);
