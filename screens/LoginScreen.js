adbef18211e3d6e00e1d1f977c2d1d256bd46aa6
Tyler Gilhuys <tg0046@mix.wvu.edu>
2024-02-17 13:14:21 -0500

    Updated Login Screen and Ensure passwords are hidden

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -1,117 +1,154 @@
-import React, {useState, useEffect} from 'react';
-import {View, StyleSheet, Text, SafeAreaView, TextInput} from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { View, StyleSheet, Text, SafeAreaView, TextInput } from 'react-native';
 import { LargeButton } from '../src/components/Buttons';
 
-function LoginScreen ({navigation}) {
-    
+function LoginScreen({ navigation }) {
+
     var [data, setData] = useState([]);
     const [name, setName] = useState("");
-    var [password, setPassword] = useState("");   
+    var [password, setPassword] = useState("");
 
     let api = "https://restapi-playerscompanion.azurewebsites.net/users/auth.php?";
-    let action='login';
+    let action = 'login';
 
-    async function sendRequest() { 
+    async function sendRequest() {
         let url = `${api}action=${action}&name=${name}&password=${password}`;
         console.log(url);
         fetch(url)
-        .then((response) => {
-            let res = response.json();
-            return res;
-        })
-        .then((json) => {
-            setData(json);
-        })
-        .catch(error => {
-            console.log("2" + error);
-        })
+            .then((response) => {
+                let res = response.json();
+                return res;
+            })
+            .then((json) => {
+                setData(json);
+            })
+            .catch(error => {
+                console.log("2" + error);
+            })
     }
-    
+
     const sendAndCont = () => {
         sendRequest();
-    
+
         console.log(data);
-        if(data.length > 45){
+        if (data.length > 45) {
+            <Text style={styles.font}>Inccorect Login Information</Text>
+
             console.log(data);
-            navigation.navigate('HomeScreen', {data});
+            navigation.navigate('HomeScreen', { data });
         }
-        else{
+        else {
+            <View>
+                <Text style={styles.font}>Inccorect Login Information</Text>
+            </View>
             navigation.navigate('LoginScreen');
         }
     }
-    
+
     return (
-        <SafeAreaView style={styles.container}>       
-        <View>
-            <Text style={styles.titlefont}>Login</Text>
+        <SafeAreaView style={styles.container}>
+            <View>
+                <Text style={styles.titlefont}>| Rehabilitation Monitoring Systems</Text>
+
+                <View style={styles.inputContainer}>
+                    <Text style={styles.font}>Email</Text>
+                    <TextInput
+                        style={[styles.input, { backgroundColor: 'white' }]}
+                        value={name}
+                        placeholder='Enter Email'
+                        onChangeText={setName}
+                    />
+                </View>
+
+                {/* <Text style={styles.titlefont}>Login</Text>
 
             <View style={styles.row}>
-                <Text style={styles.font}>Username</Text>
+                <Text style={styles.font}>Email</Text>
                 <TextInput
-                    style={styles.input}
+                    style={[styles.input, {backgroundColor: 'white'}]}
                     value={name}
                     placeholder='Enter Username'
                     onChangeText={setName}
                 />
-            </View>
-            
+            </View> */}
 
-            <View style={styles.row}>
+                {/* <View style={styles.inputContainer}>
                 <Text style={styles.font}>Password</Text>
-                <TextInput
-                    style={styles.input}
-                    value={password}
-                    placeholder='Enter Password'
-                    onChangeText={setPassword}
+                <TextInput>
+                    
+                </TextInput>
+            </View> */}
+
+                <View style={styles.inputContainer}>
+                    <Text style={styles.font}>Password</Text>
+
+                    <TextInput
+                        style={[styles.input, { backgroundColor: 'white' }]}
+                        value={password}
+                        placeholder='Enter Password'
+                        onChangeText={setPassword}
+                        secureTextEntry={true}
+                    />
+                </View>
+
+                <Text style={styles.font}></Text>
+
+                <LargeButton
+                    text='Log In'
+                    onPress={() => {
+                        setName(name);
+                        setPassword(password);
+                        sendAndCont();
+
+
+                    }}
                 />
             </View>
-
-            <Text style={styles.font}></Text>
-
-            <LargeButton 
-                text='Submit'
-                onPress={() =>  {
-                    setName(name);
-                    setPassword(password);
-                    sendAndCont();
-                 
-                  
-                }}
-            />
-        </View>
-        </SafeAreaView> 
+        </SafeAreaView>
     );
-    
+
 }
 
 export default LoginScreen;
 
 const styles = StyleSheet.create({
     container: {
-      marginTop: 50,
-      padding: 3,
-      flex: 1
+        //   marginTop: 50,
+        //   padding: 3,
+        flex: 1,
+        backgroundColor: '#AEB6C5'
     },
     row: {
-        flexDirection: "row", 
-        paddingTop: 30, 
-        justifyContent:'center'
+        flexDirection: "row",
+        paddingTop: 30,
+        justifyContent: 'flex-start',
+        alignItems: 'center'
     },
     input: {
-        height: 40,
-        marginLeft: 10,
+        height: 50,
+        borderColor: 'gray',
         borderWidth: 1,
-        padding: 10,
-        width: 170,
-        alignSelf: 'center',
+        borderRadius: 5,
+        paddingHorizontal: 10,
+        marginVertical: 5,
     },
     titlefont: {
-        fontSize: 52,
+        fontSize: 32,
         textAlign: 'center',
-        fontWeight: 'bold'
+        fontWeight: 'bold',
+        marginTop: 50,
+        marginBottom: 50,
+        color: '#1E3861',
     },
     font: {
-        fontSize: 28,
-        textAlign: 'center',
+        fontSize: 24,
+        textAlign: 'left',
+        marginLeft: 10,
+        marginBottom: 10,
+        color: '#1E3861'
     },
+    inputContainer: {
+        marginTop: 20,
+        marginHorizontal: 20,
+    }
 });
