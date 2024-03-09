ef5d477e658209f45f57e44c2ad30e60be3e3da9
Tyler Gilhuys <tg0046@mix.wvu.edu>
2024-03-09 14:04:23 -0500

    Updated Roster Screen

--- screens/althetic-trainers/RosterScreen.js
+++ screens/althetic-trainers/RosterScreen.js
@@ -1,66 +1,147 @@
-import React from 'react';
-import {View, FlatList, StyleSheet, Text, Button} from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { Searchbar } from 'react-native-paper';
+import { View, FlatList, StyleSheet, Text, Button, SafeAreaView, TextInput, Pressable, TouchableOpacity } from 'react-native';
+import { LargeButton } from '../../src/components/Buttons';
 
-const styles = StyleSheet.create({
-  container: {
-    alignItems: 'center', 
-    justifyContent: 'center',
-    marginTop:'20%',
-    marginBottom:'10%',
-    flex: 1,
-    paddingTop: 22,
-  },
-  item: {
-    padding: 50,
-    fontSize: 18,
-    height: 44,
-    width:'20%',
-  },
-  
-});
-
-//Flat, unresponsive list, maybe add more depth and player photo, more like a list on spotify
 function RosterScreen({ navigation, route }) {
-  //const sessionKey = route.params.sessionKey;
+
+  let api = "https://restapi-playerscompanion.azurewebsites.net/users/athleteLogs.php";
+  let action = 'getroster';
+  const [roster, setRoster] = useState([]);
+  const [search, setSearch] = useState('');
+  const [name, setName] = useState('');
+
+  const sendRequest = async () => {
+    let url = `${api}?action=${action}`;
+    console.log(url);
+    try {
+      const response = await fetch(url);
+      const text = await response.text(); // Get the raw response text
+      const json = JSON.parse(text); // Parse the text as JSON
+      console.log(json);
+      setRoster(json);
+      return json;
+    } catch (error) {
+      console.error("Error fetching data: ", error);
+    }
+  }
+
+  const searchFilter = (text) => {
+    const updatedData = roster.filter((item) => {
+      const item_data = '${item.firstname.toUpperCase()} ${item.lastname.toUpperCase()}';
+      const text_data = text.toUpperCase();
+      return item_data.indexOf(text_data) > -1;
+    });
+    setSearch(text);
+    setRoster(updatedData);
+  }
+
+  // issues with these lines of code, constant loading of data probably going to crash system eventually
+  useEffect(() => {
+    sendRequest();
+  }, [/**sendRequest */]);
+
+
   return (
-    <View style={styles.container}>
-       <FlatList
-        data={[
-          {key: 'Devin A'},
-          {key: 'Dan B'},
-          {key: 'Dominic C'},
-          {key: 'Jackson D'},
-          {key: 'James E'},
-          {key: 'Joel F'},
-          {key: 'John G'},
-          {key: 'Jillian H'},
-          {key: 'Jimmy I'},
-          {key: 'Julie J'},
-          {key: 'Devin K'},
-          {key: 'Dan L'},
-          {key: 'Dominic M'},
-          {key: 'Jackson N'},
-          {key: 'James O'},
-          {key: 'Joel P'},
-          {key: 'John Q'},
-          {key: 'Jillian R'},
-          {key: 'Jimmy S'},
-          {key: 'Julie T'},
-          {key: 'Devin U'},
-          {key: 'Dan V'},
-          {key: 'Dominic W'},
-          {key: 'Jackson X'},
-          {key: 'James Y'},
-          {key: 'Joel Z'},
-          {key: 'John A'},
-          {key: 'Jillian B'},
-          {key: 'Jimmy C'},
-          {key: 'Julie D'},
-        ]}
-        renderItem={({item}) => <Button title={item.key} style={styles.item} onPress={() => navigation.navigate('AthleteProfileScreen', {sessionKey :sessionKey})} />}
+    <SafeAreaView style={styles.container}>
+      <View style={styles.searchBox}>
+        <TextInput
+          style={[styles.input, { backgroundColor: 'white' }]}
+          clearButtonMode='always'
+          placeholder='Search Athlete'
+          autoCapitalize='none'
+          value={search}
+          onChangeText={(text) => searchFilter(text)}
+        />
+      </View>
+
+      <FlatList
+        style={styles.box}
+        data={roster}
+        renderItem={({ item }) =>
+          <TouchableOpacity style={styles.ath} onPress={() => navigation.navigate('AthleteProfileScreen')}>
+            <View style={styles.row}>
+              <View style={styles.circle} />
+              <Text style={styles.first}>{item.data[0]}</Text>
+              <Text style={styles.last}>{item.data[1]}</Text>
+            </View>
+          </TouchableOpacity>
+        }
       />
-    </View>
+
+      <View style={styles.button}>
+        <Text>Place buttons here</Text>
+      </View>
+
+    </SafeAreaView>
   );
 }
 
 export default RosterScreen;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#AEB6C5',
+  },
+  searchBox: {
+    marginHorizontal: 20,
+    marginBottom: 50
+  },
+  input: {
+    height: 50,
+    borderColor: 'gray',
+    borderWidth: 1,
+    borderRadius: 15,
+    paddingHorizontal: 10,
+    textAlign: 'center',
+  },
+  background: {
+    margin: 50,
+    backgroundColor: 'white',
+  },
+  ath: {
+    backgroundColor: 'white',
+    marginLeft: 25,
+    marginRight: 25,
+    marginTop: 10,
+    height: 50,
+    borderRadius: 10,
+    paddingBottom: 10,
+    ...Platform.select({
+      ios: {
+        shadowColor: '#000000',
+        shadowOffset: { width: 10, height: 10 },
+        shadowOpacity: 0.2,
+        shadowRadius: 5,
+      },
+    })
+  },
+  first: {
+    marginTop: 15,
+    marginLeft: 75
+  },
+  last: {
+    marginTop: 15,
+    marginLeft: 5
+  },
+  circle: {
+    width: 35,
+    height: 35,
+    borderRadius: 100 / 2,
+    backgroundColor: "#2C3C63",
+    marginTop: 8,
+    marginLeft: 30
+  },
+  row: {
+    flexDirection: "row",
+  },
+  box: {
+    marginBottom: 130
+  },
+  button: {
+    paddingLeft: 150,
+    textAlign: 'center'
+  }
+
+});
