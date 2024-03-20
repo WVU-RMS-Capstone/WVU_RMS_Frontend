4cf1c28a23a164b3838b0b0a096a963f87b177bd
Tyler Gilhuys <tg0046@mix.wvu.edu>
2024-03-20 17:21:35 -0400

    Updated Program Screen

--- screens/althetic-trainers/NewProgramScreen.js
+++ screens/althetic-trainers/NewProgramScreen.js
@@ -1,199 +1,268 @@
-import React from 'react';
-import { View, TextInput, StyleSheet, Text, SafeAreaView, ScrollView } from 'react-native';
+import React, { useState } from 'react';
+import { View, TextInput, StyleSheet, Text, SafeAreaView, ScrollView, TouchableOpacity, FlatList, Touchable } from 'react-native';
 import { MediumButton } from '../../src/components/Buttons';
 import { LargeButton } from '../../src/components/Buttons';
+import ImagePicker, { launchImageLibrary } from 'react-native-image-picker'
 
 // EXAMPLE: https://restapi-playerscompanion.azurewebsites.net/users/users.php?action=createRoutine&name=Legs1&IDs=4/11/13&reps=10/10/10&sets=3/4/5&visible=1
 
 
 function NewProgramScreen({ navigation, route }) {
-  let sessionKey = route.params.sessionKey;
-  const [name, setName] = React.useState('');
-  const [reps1, setReps1] = React.useState('');
-  const [sets1, setSets1] = React.useState('');
-  const [id1, setid1] = React.useState('');
-  const [reps2, setReps2] = React.useState('');
-  const [sets2, setSets2] = React.useState('');
-  const [id2, setid2] = React.useState('');
-  const [reps3, setReps3] = React.useState('');
-  const [sets3, setSets3] = React.useState('');
-  const [id3, setid3] = React.useState('');
-  const [visible, setVisible] = React.useState('');
+  // let sessionKey = route.params.sessionKey;
+  // const [name, setName] = React.useState('');
+  // const [reps1, setReps1] = React.useState('');
+  // const [sets1, setSets1] = React.useState('');
+  // const [id1, setid1] = React.useState('');
+  // const [reps2, setReps2] = React.useState('');
+  // const [sets2, setSets2] = React.useState('');
+  // const [id2, setid2] = React.useState('');
+  // const [reps3, setReps3] = React.useState('');
+  // const [sets3, setSets3] = React.useState('');
+  // const [id3, setid3] = React.useState('');
+  // const [visible, setVisible] = React.useState('');
 
-  const [assignID, setAssignID] = React.useState('');
-  const [data, setData] = React.useState('');
+  // const [assignID, setAssignID] = React.useState('');
+  // const [data, setData] = React.useState('');
 
-  const handleNameChangeText = (text) => {
-    setName(text);
+  // const handleNameChangeText = (text) => {
+  //   setName(text);
+  // };
+
+  // const createNewRoutine = () => {
+  //   var ids = id1 + '/' + id2 + '/' + id3;
+  //   var sets = sets1 + '/' + sets2 + '/' + sets3;
+  //   var reps = reps1 + '/' + reps2 + '/' + reps3;
+  //   // EXAMPLE: https://createRoutine&name=Legs1&IDs=4/11/13&reps=10/10/10&sets=3/4/5&visible=1
+  //   const api = 'https://restapi-playerscompanion.azurewebsites.net/users/users.php?action=createRoutine&name=';
+  //   var finalName = api.concat(name);
+  //   var repapi = finalName.concat('&IDs=');
+  //   var repsapi = repapi.concat(ids);
+  //   repsapi = repsapi + '&reps=' + reps + '&sets=' + sets + '&visible=' + visible;
+  //   console.log(repsapi);
+  //   fetch(repsapi, {
+  //     headers: {
+  //       'Authorization': 'Bearer ' + sessionKey
+  //     }
+  //   })
+
+  //     .then((response) => {
+  //       let res = response.json();
+  //       return res;
+  //     })
+  //     .then((json) => {
+
+  //       setData(json);
+
+  //     })
+
+  //     .catch((error) => {
+  //       console.error(error);
+  //     })
+  // }
+
+  // const sendAndCont = () => {
+  //   createNewRoutine();
+  //   console.log(data);
+  //   navigation.navigate('ATHomeScreen', { sessionKey: sessionKey });
+  // }
+
+  const [cover, setCover] = useState('');
+  const [program, setProgram] = useState('');
+  const [exercises, setExercises] = useState([]);
+  const [data, setData] = useState([]);
+  const [selectImage, setSelectImage] = useState('');
+
+  let api = "https://restapi-playerscompanion.azurewebsites.net/users/programs.php";
+  let action = 'createprogram';
+
+  async function sendRequest() {
+    let url = `${api}?action=${action}&Cover=${cover}&ProgramName=${program}&Exercise=${exercises}`;
+    console.log("Request URL: ", url);
+    try {
+      const response = await fetch(url);
+      const text = await response.text();
+      const json = JSON.parse(text);
+      setData(json);
+      return json;
+    } catch (error) {
+      console.error("Error Fetching Data: ", error);
+    }
+  }
+
+  const sendAndContune = async () => {
+    try {
+      const res = await sendRequest();
+      console.log(res)
+      navigation.navigate('ATHomeScreen');
+    } catch (error) {
+      console.error("Error Recieved: ", error);
+    }
+  }
+
+  const ImagePicker = () => {
+    let options = {
+      storageOptions: {
+        path: 'image',
+      },
+    };
+
+    launchImageLibrary(options, response => {
+      setSelectImage(response.assets[0].uri)
+      console.log(response);
+    });
   };
 
-  const createNewRoutine= () => {
-    var ids = id1 + '/' + id2 + '/' + id3;
-    var sets = sets1 + '/' + sets2 + '/' + sets3;
-    var reps = reps1 + '/' + reps2 + '/' + reps3;
-    // EXAMPLE: https://createRoutine&name=Legs1&IDs=4/11/13&reps=10/10/10&sets=3/4/5&visible=1
-    const api = 'https://restapi-playerscompanion.azurewebsites.net/users/users.php?action=createRoutine&name=';
-    var finalName = api.concat(name);
-    var repapi = finalName.concat('&IDs=');
-    var repsapi = repapi.concat(ids);
-    repsapi = repsapi + '&reps=' + reps + '&sets=' + sets + '&visible=' + visible;
-    console.log(repsapi);
-    fetch(repsapi, {
-      headers: {
-        'Authorization': 'Bearer ' + sessionKey
-       }
-    })
 
-    .then((response) => {
-      let res = response.json();
-      return res;
-  })
-  .then((json) => {
-         
-          setData(json);
-          
-      })
-    
-    .catch((error) => {
-      console.error(error);
-    })
-}
 
-const sendAndCont = () => {
-  createNewRoutine();
-  console.log(data);
-  navigation.navigate('ATHomeScreen', {sessionKey: sessionKey});
-}
+  const selectCoverImg = React.useCallback((type, options) => {
+    try {
+      // const { status } = await ImagePicker.requestMediaLibraryPermissionsAsync();
+      // if (status != 'granted') {
+      //   return;
+      // }
+
+      const result = ImagePicker.launchImageLibrary(options, setResponse);
+
+      if (!result.cancelled) {
+        setCover(result.uri);
+      }
+    } catch (error) {
+      console.error("Error: ", error);
+    }
+  }, []);
 
   return (
     <SafeAreaView style={styles.container}>
-      <TextInput style={styles.textInput}
-        underlineColorAndroid="transparent"
-        placeholder="Enter Program Name"
-        placeholderTextColor="#777777"
-        autoCapitalize="none"
-        onChangeText={setName} 
-        value = {name}/>
-    <View style={{flexDirection: "column", justifyContent: "space-evenly"}}>
-      <View style={{flexDirection: "row", justifyContent: "space-evenly", paddingTop: 0}}>
-        <View style={styles.smallBox} >
-          <Text style={styles.font}>Ex ID:</Text>
-          <TextInput style={styles.textInput2} onChangeText={setid1} value={id1}/>
-        </View>
-        <View style={styles.smallBox} >
-          <Text style={styles.font}>Sets:</Text>
-          <TextInput style={styles.textInput2} onChangeText={setSets1} value={sets1}/>
-        </View>
-        <View style={styles.smallBox} >
-          <Text style={styles.font}>Reps:</Text>
-          <TextInput style={styles.textInput2} onChangeText={setReps1} value={reps1}/>
-        </View>
+      <View style={[styles.row]}>
+        <TouchableOpacity style={styles.coverImg} onPress={() => {
+          ImagePicker();
+        }}>
+          <Text>Insert Cover</Text>
+        </TouchableOpacity>
+        <TextInput
+          style={[styles.input, { backgroundColor: 'white' }]}
+          value={program}
+
+          placeholder='Program Name'
+          onChangeText={setProgram}
+        />
       </View>
 
+      <FlatList
+        style={styles.box}
+        data={[
+          { key: 'RDL' },
+          { key: 'Lateral Band Movements' },
+          { key: 'Box Jumps' },
+          { key: 'Step Downs' }
+        ]}
+        exercise={data}
+        renderItem={({ item }) =>
+          <View style={styles.exercise}>
+            <Text style={styles.exerciseText}>{item.key}</Text>
+          </View>
+        }
+      />
 
-      <View style={{flexDirection: "row", justifyContent: "space-evenly", paddingTop: 20}}>
-        <View style={styles.smallBox} >
-          <Text style={styles.font}>Ex ID:</Text>
-          <TextInput style={styles.textInput2} onChangeText={setid2} value={id2}/>
-        </View>
-        <View style={styles.smallBox} >
-          <Text style={styles.font}>Sets:</Text>
-          <TextInput style={styles.textInput2} onChangeText={setSets2} value={sets2}/>
-        </View>
-        <View style={styles.smallBox} >
-          <Text style={styles.font}>Reps:</Text>
-          <TextInput style={styles.textInput2} onChangeText={setReps2} value={reps2}/>
-        </View>
+      <View style={styles.row}>
+        <TouchableOpacity onPress={() => sendAndContune()}>
+          <View style={styles.button}>
+            <Text style={styles.buttonText}>Done</Text>
+          </View>
+        </TouchableOpacity>
+        <TouchableOpacity onPress={() => navigation.navigate('ExercisesScreen')}>
+          <View style={styles.button}>
+            <Text style={styles.buttonText}>Add Exercises</Text>
+          </View>
+        </TouchableOpacity>
       </View>
+    </SafeAreaView >
 
-
-      <View style={{flexDirection: "row", justifyContent: "space-evenly", paddingTop: 20}}>
-        <View style={styles.smallBox} >
-          <Text style={styles.font}>Ex ID:</Text>
-          <TextInput style={styles.textInput2} onChangeText={setid3} value={id3}/>
-        </View>
-        <View style={styles.smallBox} >
-          <Text style={styles.font}>Sets:</Text>
-          <TextInput style={styles.textInput2} onChangeText={setSets3} value={sets3}/>
-        </View>
-        <View style={styles.smallBox} >
-          <Text style={styles.font}>Reps:</Text>
-          <TextInput style={styles.textInput2} onChangeText={setReps3} value={reps3}/>
-        </View>
-      </View>
-    
-      <View style = {{flexDirection: "row", justifyContent: "space-evenly", paddingTop: 20}}> 
-        <View style={styles.box} >
-          <Text style={styles.font}>Assign User ID:</Text>
-          <TextInput style={styles.textInput2} onChangeText={setAssignID} value={assignID}/>
-        </View>
-        <View style={styles.box} >
-          <Text style={styles.font}>Visible (1/0):</Text>
-          <TextInput style={styles.textInput2} onChangeText={setVisible} value={visible}/>
-        </View>
-      </View>
-
-      </View>
-      
-      <View style={{paddingTop: 30}}>
-        <LargeButton  text="Done" onPress={() => sendAndCont()} />
-        </View>
-
-        </SafeAreaView>
- 
   );
 }
-    
+
 export default NewProgramScreen;
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center',
-    
+    backgroundColor: '#AEB6C5'
+
   },
-  textInput: {
-    alignSelf: 'center',
-    backgroundColor: '#D9D9D9',
-    borderRadius: 14,
-    fontSize: 40,
-    paddingVertical: "1%",
-    paddingHorizontal: "5%",
-    marginBottom: '8%',
-    marginTop:'5%',
-    height: "15%",
-    width: "93%"
-  },
-  font: {
-    fontSize: 22,
-    marginRight: 10,
-  },
-  textInput2: {
-    fontSize: 24,
-    flex: 1,
-  },
-  smallBox: {
-    borderRadius: 5,
-    padding: 10,
-    width: '32%',
-    height: 103,
+  coverImg: {
+    backgroundColor: 'white',
+    width: '40%',
+    height: 100,
     borderRadius: 15,
-    flexDirection: 'row',
     alignItems: 'center',
-    backgroundColor: '#D9D9D9',
-    
+    justifyContent: 'center'
+  },
+  row: {
+    flexDirection: 'row',
+    justifyContent: 'space-evenly'
+  },
+  input: {
+    height: 40,
+    marginTop: 30,
+    borderRadius: 15,
+    width: '40%',
+    textAlign: 'center',
   },
   box: {
-    borderRadius: 5,
-    padding: 10,
-    width: '45%',
-    height: 75,
-    borderRadius: 15,
-    flexDirection: 'row',
-    alignItems: 'center',
-    textAlign:'center',
-    alignSelf: 'center',
-    fontSize: 30,
-    fontWeight: '500',
-    backgroundColor: '#D9D9D9',
+    backgroundColor: 'white',
+    margin: 30,
+    marginBottom: 50,
+    borderRadius: 15
   },
+  button: {
+    borderRadius: 15,
+    borderWidth: 1,
+    borderColor: "#757575",
+    paddingBottom: 15,
+    paddingVertical: 5,
+    justifyContent: 'flex-end',
+    width: 150,
+    height: 60,
+    backgroundColor: '#1E3861',
+    alignSelf: 'center',
+
+    ...Platform.select({
+      ios: {
+        shadowColor: '#000000',
+        shadowOffset: { width: 10, height: 10 },
+        shadowOpacity: 0.2,
+        shadowRadius: 5,
+      },
+    })
+  },
+  buttonText: {
+    color: 'white',
+    fontSize: 20,
+    // fontWeight: "500",
+    textAlign: 'center',
+    // paddingVertical: "1%",
+  },
+  exercise: {
+    borderRadius: 15,
+    borderColor: "#757575",
+    marginTop: 15,
+    justifyContent: 'center',
+    width: '90%',
+    height: 60,
+    backgroundColor: '#1E3861',
+    alignSelf: 'center',
+
+    ...Platform.select({
+      ios: {
+        shadowColor: '#000000',
+        shadowOffset: { width: 10, height: 10 },
+        shadowOpacity: 0.2,
+        shadowRadius: 5,
+      },
+    })
+  },
+  exerciseText: {
+    color: 'white',
+    fontSize: 20,
+    textAlign: 'center',
+  }
 });
